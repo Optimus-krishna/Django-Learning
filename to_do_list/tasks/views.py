433d5e7b9b15d1c7ec969dd2b3b9bb7a829de54f
Krishna <krishnaacharya.biratnagar@gmail.com>
2021-12-30 12:12:01 +0545

    add tasks app

--- to_do_list/tasks/views.py
+++ to_do_list/tasks/views.py
@@ -4,7 +4,10 @@ tasks = ["foo", "bar", "baz"]
 
 # Create your views here.
 
-def index(rquest):
+def index(request):
     return render(request, "tasks/index.html", {
         "tasks":tasks
     })
+
+def add(request):
+    return render(request, "tasks/add.html")
